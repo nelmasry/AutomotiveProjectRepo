37926ac22fcb9f00d6d18bba0fdacfa9f68bb172
nader.126 <nader.126@hotmail.com>
2019-11-06 21:34:45 +0200

    Initial Commit

---
+++ CustomerAPI/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Customers list";
+        }
+    }
+}
---
+++ VehicleAPI/Controllers/VehicleController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VehicleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Vehicles list";
+        }
+    }
+}
