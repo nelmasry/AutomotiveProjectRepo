59a16420601c4425e67838d7112899f22fe3ab84
nader.126 <nader.126@hotmail.com>
2019-11-15 13:59:50 +0200

    Test failed tests rollback

--- VehicleAPI.Tests/VehicleControllerTest.cs
+++ VehicleAPI.Tests/VehicleControllerTest.cs
@@ -74,7 +74,7 @@ namespace CustomerAPI.Tests
             var model = okObjectResult.Value as IEnumerable<Vehicle>;
             Assert.NotNull(model);
             int count = model.Count();
-            Assert.Equal(1, count);
+            Assert.Equal(7, count);
         }
         [Fact]
         public void GetCutomerVehicles()
